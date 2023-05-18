0fcb949069a0508c2194f1949424010b15e31972
Isaac José <106356687+Isaac-SZ@users.noreply.github.com>
2023-05-18 20:03:32 -0300

    Teste de BD

---
+++ bd.py
@@ -0,0 +1,42 @@
+lojas = [
+    {
+        "id": 1,
+        "Nome Fantasia": "Ecoloja",
+        "Razao Social": "Ecoloja SA",
+        "CNPJ": "22.206.183/0001-39",
+        "Endereco": "Rua das Ninfas, 281 - Soledade, Recife",
+        "Redes Sociais": "@ecoloja"
+    },
+    {
+        "id": 2,
+        "Nome Fantasia": "Sabor Natural",
+        "Razao Social": "Sabor Natural LTDA",
+        "CNPJ": "34.937.499/0001-80",
+        "Endereco": "Rua Joaquim Tenorio, 324 - Cavaleiro, Jaboatão dos Guararapes",
+        "Redes Sociais": "@sabor.natural"
+    },
+    {
+        "id": 3,
+        "Nome Fantasia": "Verde Vivo",
+        "Razao Social": "Verde Vivo SA",
+        "CNPJ": "67.151.470/0001-27",
+        "Endereco": "Rua Deolindo Tavares, 263 - Imbiribeira, Recife",
+        "Redes Sociais": "@verdevivo"
+    },
+    {
+        "id": 4,
+        "Nome Fantasia": "Essência Orgânica",
+        "Razao Social": "Essência Orgânica SS",
+        "CNPJ": "24.039.585/0001-11",
+        "Endereco": "Rua José Osório, 580 - Madalena, Recife",
+        "Redes Sociais": "@essenciaorganica"
+    },
+    {
+        "id": 5,
+        "Nome Fantasia": "Raízes Sustentáveis",
+        "Razao Social": "Raízes Sustentáveis LTDA",
+        "CNPJ": "90.741.494/0001-58", "Endereco": "Avenida Barão de Bonito, 1110 - Várzea, Recife",
+        "Redes Sociais": "@raizessustentaveis"
+
+    }
+]
